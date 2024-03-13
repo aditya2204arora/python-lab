05aa1a22951a08a527a05ea35596cce54a278d09
aditya2204arora <160465285+aditya2204arora@users.noreply.github.com>
2024-03-13 09:08:34 +0000

    new files added

---
+++ func.py
@@ -0,0 +1,13 @@
+def addition(n1,n2):
+    n3=n1+n2
+    return n3
+
+num1=int(input())
+num2=int(input())
+print(addition(num1,num2))
+
+def myname():
+    return "Hello"
+
+name=input("your name: ")
+print(myname() + " " + name)
