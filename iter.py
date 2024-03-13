2f89964cb7c6cf8f56b6e1093813f2ef41bd7108
aditya2204arora <160465285+aditya2204arora@users.noreply.github.com>
2024-03-13 10:58:27 +0000

    generator.py

--- iter.py
+++ iter.py
@@ -1,8 +1,15 @@
 mylst=[1,3,5,2,3]
 i=iter(mylst)
+while True:
+    try:
+        print(next(i))
+    except StopIteration:
+        break
 
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,16 @@
-print((lambda x:(x+3)*5/2)(3))
+# def even():
+#     evnlist=[]
+#     for i in range(20):
+#         if i%2==0:
+#             evnlist.append(i)
+#     return evnlist
+
+# print(even())
+
+def even():
+    for i in range(20):
+        if i%2==0:
+            return i
+
+for i in even():
+    print(i)
